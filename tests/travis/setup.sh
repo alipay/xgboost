e7746f70683b97e821ad66f6458eaab00844afee
sperlingxx <lovedreamf@gmail.com>
2019-07-03 22:29:14 +0800

    fix travis config

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -8,7 +8,7 @@ if [ ${TASK} == "python_test" ] || [ ${TASK} == "launcher_test" ]; then
     fi
     bash conda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
-    hash -ra
+    hash -r
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
     # Useful for debugging any issues with conda
