58b1cf1dae77bbb0d8902e7a83d44d9e601596b1
sperlingxx <lovedreamf@gmail.com>
2019-08-02 23:51:17 +0800

    fix

--- xgboost-launcher/tests/test_model_helper.py
+++ xgboost-launcher/tests/test_model_helper.py
@@ -10,7 +10,8 @@ register_model_source('local', None, LocalModelSource)
 
 
 def test_launcher_model():
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_resources/test_booster')
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(file_path, 'test_resources/test_booster')
     model = load_launcher_model(ModelFields(model_path=path))
     assert isinstance(model.meta, XGBoostTrainFields)
     params = model.meta.params
@@ -23,9 +24,10 @@ def test_launcher_model():
     assert model.meta.num_boost_round == 100
 
     meta_cpy = model.meta._asdict()
-    if not os.path.exists('test_resources/tmp/'):
-        os.mkdir('test_resources/tmp/')
-    tmp_path = mktemp(prefix='test_resources/tmp/')[5:]
+    tmp_path = os.path.join(file_path, 'test_resources/tmp/')
+    if not os.path.exists(tmp_path):
+        os.mkdir(tmp_path)
+    tmp_path = mktemp(prefix=tmp_path)[5:]
     save_launcher_model(
         model.booster,
         config_fields.TrainFields(model.meta, None, ModelFields(model_path=tmp_path)))
